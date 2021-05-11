28043fa66bcdb1384ec300397ce468d316c57cf0
khady mbacke Diop <68691677+Khady199@users.noreply.github.com>
2021-05-11 03:58:48 +0000

    mise à jour

---
+++ meet_analyser.sql
@@ -0,0 +1,76 @@
+
+create database GoogleAnalyzer;
+use GoogleAnalyzer;
+
+CREATE  TABLE Classe (
+    CodeClasse VARCHAR(50) PRIMARY KEY,
+    NombreEtudiant INT,
+    idEtudiant VARCHAR(30),
+   CONSTRAINT fk_Clas_idEtudiant_Etudi FOREIGN KEY(idEtudiant)
+    REFERENCES Etudiant(idEtudiant)
+);
+
+CREATE  TABLE Matiere (
+    IdMatiere VARCHAR(50) PRIMARY KEY,
+    NomMatiere VARCHAR(50),
+    Coefficient INT
+);
+
+
+
+CREATE TABLE Etudiant (
+    idEtudiant VARCHAR(30) PRIMARY KEY,
+    NomEtudiant VARCHAR(50),
+    PrenomEtudiant VARCHAR(50),
+    Email VARCHAR(50)
+);
+
+
+CREATE  TABLE Choix_Etudi_Matier(
+     IdEtudiant VARCHAR(30),
+     IdMatiere VARCHAR(50),
+     NombreDeChoix INT
+ );
+
+CREATE  TABLE Seance (
+    IdSeance INT auto_increment PRIMARY KEY,
+    DateDebut DATE,
+    DateFin DATE,
+    HeureDebut TIME,
+    HeureFin TIME,
+    Duree TIME
+);
+
+CREATE  TABLE EvaluSeance (
+    idEtudiant VARCHAR(30),
+    idSeance INT,
+    NombrEvaluation INT
+);
+
+CREATE TABLE Professeur(
+    Matricule VARCHAR (50) PRIMARY KEY,
+    NomProf VARCHAR(50),
+    PrenomProf VARCHAR(50),
+    Email VARCHAR(100),
+    idMatiere VARCHAR(50),
+    CodeClasse VARCHAR(50),
+    CONSTRAINT  fk_Prof_idMatiere_Matie FOREIGN KEY(IdMatiere)
+    REFERENCES Matiere(IdMatiere) ON DELETE CASCADE,
+    CONSTRAINT fk_Prof_CodeClasse_Class FOREIGN KEY(CodeClasse)
+    REFERENCES Classe(CodeClasse)
+);
+
+-- l'insertion se fera automatiquement 
+
+--donnons les privilèges à l'utilisateur Professeur
+
+create user Professeur@'%' idenfied by "passer";
+grant select on GoogleAnalyzer.Classe to Professeur@'localhost';
+grant all privilèges on GoogleAnalyzer.Seance to Professeur@'localhost';
+
+--donnons les privilèges à l'utilisateur Etudiant
+
+create user Etudiant@'%' idenfied by "passer";
+grant select on GoogleAnalyzer.* to Etudiant@'localhost';
+
+
