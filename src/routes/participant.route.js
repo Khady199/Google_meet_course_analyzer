e61b46ab7d76f3b1cc33b9400c3850b9ee1b3c6c
khady mbacke Diop <68691677+Khady199@users.noreply.github.com>
2021-05-11 03:56:13 +0000

    mise à jour

--- src/routes/participant.route.js
+++ src/routes/participant.route.js
@@ -1,23 +1,23 @@
 /**
- * Router file for the seance object
- * This file contains method for handling
- * GET and POST request for the participant
+ *Fichier routeur pour l'objet seance
+ * Ce fichier contient une méthode pour traiter
+ * les requêtes GET et POST pour le participant
  */
  const router = require("express").Router();
  const { getParticipantById, addParticipant, getAllParticipant } = require("../service/participant.service");
  
  router
    .get("/participant", async (req, res) => {
-     //get all participants 
+     //obtenir tous les participants (get all participants) 
      res.send(await getAllParticipant());
    })
    .get("/participant/:id", async (req, res) => {
-     //get participant by id
+     //obtenir un participant par identifiant( get participant by id)
      const participant = await getParticipantById(req.params.id);
      res.send(participant);
    })
    .post("/participant", async (req, res) => {
-     //post method for adding participant
+     //méthode post pour ajouter un participant
      const { duration_seconds, device_type, email_address, location_region} = req.body;
      let result = await addParticipant({ duration_seconds, device_type,email_address, location_region });
      res.send(result);
--- src/routes/seance.route.js
+++ src/routes/seance.route.js
@@ -1,23 +1,23 @@
 /**
- * Router file for the seance object
- * This file contains method for handling
- * GET and POST request for the seances
+ Fichier routeur pour l'objet seance
+ * Ce fichier contient une méthode pour traiter
+ * les requêtes GET et POST pour la seance
  */
 const router = require("express").Router();
 const { getSeanceById, addSeance, getAllSeance } = require("../service/seance.service");
 
 router
   .get("/seance", async (req, res) => {
-    //get all seances
+    //obtenir tous une seance (get all  seance) 
     res.send(await getAllSeance());
   })
   .get("/seance/:id", async (req, res) => {
-    //get seance by id
+    //obtenir une seance par identifiant
     const seance = await getSeanceById(req.params.id);
     res.send(seance);
   })
   .post("/seance", async (req, res) => {
-    //post method for adding seance
+    //méthode post pour ajouter une seance
     const { conference_id, startDate, endDate, hasScreenShared } = req.body;
     let result = await addSeance({ conference_id, startDate, endDate, hasScreenShared });
     res.send(result);
