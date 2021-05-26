59cb1d7cdaedc13d9188298ac917db98478aeec9
Khady199 <68691677+Khady199@users.noreply.github.com>
2021-05-26 20:46:41 +0200

    connexion à la BD

---
+++ noteS.php
@@ -0,0 +1,6 @@
+<?php
+    $not= $_GET['r1']+$_GET['r2']+$_GET['r3']+$_GET['r4']+$_GET['r5']+$_GET['r6']+$_GET['r7']+$_GET['r8']+$_GET['r9']+$_GET['r11']+$_GET['r12']+$_GET['r13']+$_GET['r14']+$_GET['r15']+$_GET['r16']+$_GET['r17']+$_GET['r18'];
+    echo 20*$not/58;
+
+    
+?>
