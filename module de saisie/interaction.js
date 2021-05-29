0e96c5d2e8ff7ea5bbb9ba088ec004cea97f0518
Khady199 <68691677+Khady199@users.noreply.github.com>
2021-05-29 20:56:42 +0200

    Update interaction.js

--- module de saisie/interaction.js
+++ module de saisie/interaction.js
@@ -1,14 +1,14 @@
 //console.log("Extension")
 
-//creeons une fonction capable de selectionner n'importe quel données 
+//créeons une fonction capable de selectionner n'importe quelles données 
 
 function getRecupere()
 {
-    // on va tester si la fenetre nous permet de recuperer les données
+    // on va tester si la fenêtre nous permet de recuperer les données
     if(window.getRecupere)
     {
-        // obtention des données recuperer dans cette fenetre 
-        // et appeler la fonction toString pour voir le contenu
+        // obtention des données recuperées dans cette fenêtre 
+        // et appel de la fonction toString pour voir le contenu
         console.log(window.getRecupere().toString())
     }
 }
