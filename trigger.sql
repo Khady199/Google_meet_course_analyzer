97579b41e962b799040da2d9ce850ec66a9cd557
Khady199 <68691677+Khady199@users.noreply.github.com>
2021-05-29 02:21:13 +0200

    Update du trigger

--- trigger.sql
+++ trigger.sql
@@ -1,14 +1,15 @@
-CREATE TRIGGER evaluation  AFTER INSERT
- --creation du trigger
-   
-      --AFTER INSERT sert à définir quand et comment le trigger est déclenché 
-  
-    ON TABLE evalution FOR EACH ROW
-       --on définit sur quelle table le trigger est attaché
-     -- FOR EACH ROW signifie 'pour chaque ligne ' exemple sur chaque ligne insérée/supprimer/modifier
-      
-        SELECT avg(note_evaluation) FROM evaluation;
-           --on calcul la moyenne des note de l'evaluation dans la table evaluation
+-- Triggers `Reponse`
+--
+DELIMITER $$
+CREATE TRIGGER `Evaluation` AFTER INSERT ON `Reponse` FOR EACH ROW BEGIN
+SET @NoteEvaluation = ((new.r1+new.r2+new.r3+new.r4+new.r5+new.r6+new.r7+new.r8+new.r9+new.r10+new.quest11+new.r12+new.r13+new.r14+new.r15+new.r16+new.r17+new.r18)*20)/72;
+SET @IdEtudiant = new.IdEtudiant;
+SET @IdSeance = new.IdSeance;
+SET @suggestion = new.suggestion;
+INSERT INTO evaluation VALUES (@IdEtudiant, @IdSeance, @NoteEvaluation, @suggestion);
+END
+$$
+DELIMITER ;
             
          
             
