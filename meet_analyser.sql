780dc3746f8cecbb56a5af7c2d38428280bd4d69
Khady199 <68691677+Khady199@users.noreply.github.com>
2021-05-29 02:18:32 +0200

    upate

--- meet_analyser.sql
+++ meet_analyser.sql
@@ -44,7 +44,8 @@ CREATE TABLE Etudiant (
 CREATE  TABLE Evaluation (
     IdEtudiant INT primary key,
     IdSeance INT,
-    NombrEvaluation INT,
+    NoteEvaluation INT,
+    Suggestion VARCHAR(50),
     CONSTRAINT fk_Eval_Etud FOREIGN KEY (IdEtudiant)
     REFERENCES Classe (IdEtudiant),
     CONSTRAINT fk_Eval_Seance FOREIGN KEY (IdSeance)
@@ -65,4 +66,41 @@ grant all privilèges on meet_analyzer.Seance to Professeur@'localhost';
 create user Etudiant@'%' idenfied by "passer";
 grant select on meet_analyzer.* to Etudiant@'localhost';
 
+-- Table structure for table `Reponse`
+CREATE TABLE Reponse(
+  r1 int(4) NOT NULL,
+  r2 int(4) NOT NULL,
+  r3 int(4) NOT NULL,
+  r4 int(4) NOT NULL,
+  r5 int(4) NOT NULL,
+  r6 int(4) NOT NULL,
+  r7 int(4) NOT NULL,
+  r8 int(4) NOT NULL,
+  r9 int(4) NOT NULL,
+  r10 int(4) NOT NULL,
+  r11 int(4) NOT NULL,
+  r12 int(4) NOT NULL,
+  r13 int(4) NOT NULL,
+  r14 int(4) NOT NULL,
+  r15 int(4) NOT NULL,
+  r16 int(4) NOT NULL,
+  r17 int(4) NOT NULL,
+  r18 int(4) NOT NULL,
+  `suggest` varchar(1000) NOT NULL,
+  `idpart` varchar(255) NOT NULL,
+  `idsea` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+-- Triggers `Reponse`
+--
+DELIMITER $$
+CREATE TRIGGER `meet` AFTER INSERT ON `Reponse` FOR EACH ROW BEGIN
+SET @NoteEvaluation = ((new.r1+new.r2+new.r3+new.r4+new.r5+new.r6+new.r7+new.r8+new.r9+new.r10+new.quest11+new.r12+new.r13+new.r14+new.r15+new.r16+new.r17+new.r18)*20)/72;
+SET @IdEtudiant = new.IdEtudiant;
+SET @IdSeance = new.IdSeance;
+SET @suggestion = new.suggestion;
+INSERT INTO evaluation VALUES (@IdEtudiant, @IdSeance, @NoteEvaluation, @suggestion);
+END
+$$
+DELIMITER ;
